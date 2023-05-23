3d060260d6c6cc932018068448f6b84791feca5e
DavidRossHall <34731048+DavidRossHall@users.noreply.github.com>
2023-05-23 16:27:32 -0400

    dabbled with css

--- app.R
+++ app.R
@@ -27,15 +27,29 @@ pfas_tox <- read_csv(file = "www/jiajun_EHP2020.csv")
 # 2. UI  -----
 
 ui <- fluidPage(
-  navbarPage("My Application",
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style_edits.css")
+  ), 
+  navbarPage(title = div(
+    div(
+      id = "img-id",
+      img(src = "images/tempIcon.png", style = "width:50%")
+    ),
+    "PFAStlas"
+  ),
+  
+  # Insert rest of ui code
                  tabPanel("Welcome",
                           includeHTML("www/welcome.html")),
                  tabPanel("Toxicity",
-                          sidebarPanel(DT::dataTableOutput('pfasTable')),
+                          sidebarPanel(
+                            p("You can interact with the time series plot above; i.e. narrowing displayed date range. Note however that the date range for both plots is dictated by your inputted dates. In other words, you’ll need to change your inputted dates to update the data displayed on the correlation plot. To save as an image, use the download button in the top-left (time-series) or right-click and save as (correlation)"),
+                            DT::dataTableOutput('pfasTable')),
                           mainPanel(textOutput("selected_var"))),
                  navbarMenu("Proteomics",
                             tabPanel("Global Changes"),
                             tabPanel("Chemical-Centric",
+                                     
                                      sidebarPanel(DT::dataTableOutput('pfasTable2')),
                                      mainPanel()),
                             tabPanel("Protein-centric")
