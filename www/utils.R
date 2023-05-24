dcfd4c8eb09567387800bb4ee43cb66a803e81bf
DavidRossHall <34731048+DavidRossHall@users.noreply.github.com>
2023-05-24 13:10:41 -0400

    trying to get UI elements for plotting sorted out.

--- www/utils.R
+++ www/utils.R
@@ -1,4 +1,7 @@
 ### Created by David Ross Hall --- davidross.hall@mail.utoronto.ca
 ### File created on 2022-11-0
 
-
+# removes HTML tags from string
+cleanHTML <- function(htmlString) {
+  return(gsub("<.*?>", "", htmlString))
+}
